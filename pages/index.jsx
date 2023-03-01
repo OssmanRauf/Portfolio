c204a146b1355366312a477266460bd6c63b27a5
ossman rauf <ossmanrauf8@gmail.com>
2023-03-01 13:25:10 +0200

    adding favicon and tittle

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,17 +4,25 @@ import Portfolio from "../components/Portfolio";
 import AboutMe from "../components/AboutMe";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import Head from "next/head";
 export default function Home() {
 	return (
-		<div className="scroll-smooth bg-slate-900 text-gray-300 w-full">
-			<NavBar />
-			<div className="max-w-5xl w-11/12 mx-auto">
-				<Introduction />
-				<Myskills />
-				<Portfolio />
-				<AboutMe />
-				<Footer />
+		<>
+			<Head>
+				<title>Ossman Rauf</title>
+				<meta name="description" content="My Portfolio" />
+				<link rel="icon" type="image/x-icon" href="/assets/avatar.ico"></link>
+			</Head>
+			<div className="scroll-smooth bg-slate-900 text-gray-300 w-full">
+				<NavBar />
+				<div className="max-w-5xl w-11/12 mx-auto">
+					<Introduction />
+					<Myskills />
+					<Portfolio />
+					<AboutMe />
+					<Footer />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
