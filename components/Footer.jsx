0a06348cba15c43d546367f49167633686a27f41
ossman rauf <ossmanrauf8@gmail.com>
2023-03-02 20:36:29 +0200

    adjusting footer info

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,9 +7,7 @@ const Footer = () => {
 				<h1 className="text-2xl font-semibold py-1">Ossman Rauf</h1>
 			</a>
 			<h2 className="text-md py-2">Ossmarrauf@gmail.com</h2>
-			<p className="text-xs mt-2 text-gray-500">
-				© TechHub-Blog Creative 2023. All rights reserved
-			</p>
+			<p className="text-xs mt-2 text-gray-500">© Ossman Rauf 2023</p>
 		</div>
 	);
 };
